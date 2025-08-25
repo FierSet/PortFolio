43f42a30979b2e68834f195d179b781e43191eb6
FierSet <miguel-011998@hotmail.com>
2025-08-24 22:26:47 -0600

    resize media at css

--- obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2621a66293293d7976857f4376873f80ce3cb308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be140428fadb8e13f8f553a6216fe2e98c6fa004")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
