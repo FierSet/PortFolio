c8c94522af490f63a44e1f96f9e2acb08bc533a9
FierSet <miguel-011998@hotmail.com>
2025-08-29 21:01:47 -0600

    add animation

--- obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+facf513e5a3ae0d252b532e9347d332880b8ef8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29fc7e2bf723ba1c6f5766cfc889a57276e3df9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
