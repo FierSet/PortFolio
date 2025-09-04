456ebf42cf43c96e692ec178ff16d8427c3d8e93
FierSet <miguel-011998@hotmail.com>
2025-09-03 21:18:01 -0600

    show by default IA

--- obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b5ce676f56e0a6da1050da2606a8fdeaa23ed5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1232f362ca3d5fc34348455dd5262b0bb5f43a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
