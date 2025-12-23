32d5012e1290e7b85ac4833ffae6be18e1b67ac2
miguel angel <fiercemaoz@gmail.com>
2025-12-22 18:08:33 -0600

    solve version error

--- obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortFolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae687e7a895dec2ea69d77c1efbd9c4ac25e0e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288063e7fce380fb07b806b2fa196664b546f6e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
