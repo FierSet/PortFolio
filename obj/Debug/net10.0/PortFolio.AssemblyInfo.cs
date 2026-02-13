7e782a668415520f22e1cb0db0ed59df3d8bf750
Miguel <miguel-011998@hotmail.com>
2026-02-13 12:56:46 -0600

    update from .net9 to .net10 on PipeLine

--- obj/Debug/net10.0/PortFolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortFolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18558cedb0b5edf1526a1878614b5bd6587ff7e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e872b502b3fdf9e010f7f7f39f7cebc2f23a7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
