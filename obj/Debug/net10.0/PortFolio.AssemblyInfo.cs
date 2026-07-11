b0ef24eb6887a5e5a0308b95a9e60751670309da
Miguel <miguel-011998@hotmail.com>
2026-07-11 13:08:28 -0600

    remove template

--- obj/Debug/net10.0/PortFolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/PortFolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e872b502b3fdf9e010f7f7f39f7cebc2f23a7d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e782a668415520f22e1cb0db0ed59df3d8bf750")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortFolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
